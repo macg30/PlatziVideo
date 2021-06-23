090c81204d7cc56ea33760141fb8df2ccd40b9cc
macg30 <macg30@users.noreply.github.com>
2021-06-23 23:28:20 +0200

    Initial config

---
+++ src/component/HelloWolrd.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../assets/styles/App.scss'
+
+const HelloWolrd = () => (
+    <h1>Hola Mundo</h1>
+);
+
+export default HelloWolrd
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWolrd from './component/HelloWolrd.jsx'
+
+ReactDOM.render(<HelloWolrd/>, document.getElementById('app'));
