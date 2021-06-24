17662bbd0af24d619af22c33a5dec17216cfa33e
macg30 <macg30@users.noreply.github.com>
2021-06-24 14:59:31 +0200

    Add Carousel group components, images with webpack config

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import Header from "../components/Header.jsx";
 import Search from "../components/Search.jsx";
+import Categories from "../components/Categories.jsx";
+import Carousel from "../components/Carousel.jsx";
+import CarouselItem from "../components/CarouselItem.jsx";
+import Footer from "../components/Footer.jsx";
 import '../assets/styles/App.scss'
 
 const App = () => (
     <div className="App">
         <Header/>
         <Search/>
+        <Categories title="Mi Lista">
+            <Carousel>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+            </Carousel>
+        </Categories>
+
+        <Categories title="Tendencias">
+            <Carousel>
+                <CarouselItem/>
+                <CarouselItem/>
+            </Carousel>
+        </Categories>
+
+        <Categories title="Originales de Platzi Video">
+            <Carousel>
+                <CarouselItem/>
+            </Carousel>
+        </Categories>
+        <Footer/>
     </div>
 );
 
