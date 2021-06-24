1126b47e557e4190c50205e80f9a71c445d9cf6e
macg30 <macg30@users.noreply.github.com>
2021-06-24 11:21:19 +0200

    Add Search component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header.jsx";
+import Search from "../components/Search.jsx";
 import '../assets/styles/App.scss'
 
 const App = () => (
     <div className="App">
         <Header/>
+        <Search/>
     </div>
 );
 
