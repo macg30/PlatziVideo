7abee9b5c5f789ba511fd256144abeb7d15bd844
macg30 <macg30@users.noreply.github.com>
2021-06-27 16:06:26 +0200

    Add Register component and Switch from router

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter,Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home.jsx'
 import Login from '../containers/Login.jsx';
+import Register from '../containers/Register.jsx';
 
 const App = () => (
     <BrowserRouter>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+        </Switch>
     </BrowserRouter>
 );
 
