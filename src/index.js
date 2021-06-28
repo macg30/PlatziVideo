cf132781c146fffb366f74aa3f3cc3fce794949f
macg30 <macg30@users.noreply.github.com>
2021-06-28 23:46:49 +0200

    Initial structure for Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { CreateStore } from 'redux';
 import App from './routes/App.js';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>, 
+    document.getElementById('app')
+);
