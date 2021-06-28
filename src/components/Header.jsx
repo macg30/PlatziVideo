4c070d12b88a26d85933d04f57279d6cfb53b23b
macg30 <macg30@users.noreply.github.com>
2021-06-28 23:44:11 +0200

    Add Link tags in components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import '../assets/styles/components/Header.scss'
 import logo from '../assets/static/logo-platzi-video-BW2.png'
 import userIcon from '../assets/static/user-icon.png'
 
 const Header = () => (
     <header className="header">
+    <Link to="/">
+      <img className="header__img" src={logo} alt="Platzi Video"/>
+    </Link>
+
+    <a href="/">
     <img className="header__img" src={logo} alt="Platzi Video"/>
+    </a>
+  
     <div className="header__menu">
       <div className="header__menu--profile">
         <img src={userIcon} alt=""/>
         <p>Perfil</p>
       </div>
       <ul>
-        <li><a href="/">Cuenta</a></li>
-        <li><a href="/">Cerrar Sesión</a></li>
+        <li>
+          <Link to="/">
+            Cuenta
+          </Link>
+        </li>
+        <li>
+          <Link to="/login">
+            Iniciar sesión
+          </Link>
+        </li>
       </ul>
     </div>
   </header>
