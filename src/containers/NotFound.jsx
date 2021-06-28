952050e56f676d835140c60abe88f256f310653c
macg30 <macg30@users.noreply.github.com>
2021-06-28 21:23:42 +0200

    Add NotFound component

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/components/NotFound.scss'
+
+const NotFound = () => (
+    <>
+        <section class="error">
+            <h2 class="error__code">404</h2>
+            <p class="error__message" >Página no encontrada</p>
+        </section>
+    </>
+);
+
+export default NotFound;
--- src/routes/App.js
+++ src/routes/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter,Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home.jsx'
 import Login from '../containers/Login.jsx';
 import Register from '../containers/Register.jsx';
+import NotFound from '../containers/NotFound.jsx';
 
 const App = () => (
     <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () => (
             <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
+            <Route component={NotFound} />
         </Switch>
     </BrowserRouter>
 );
