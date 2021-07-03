669a7dd07fa604ba0b3794127115f418748f2432
macg30 <macg30@users.noreply.github.com>
2021-07-04 01:03:34 +0200

    Add condiction for CarouselItem in 'Mi Lista'

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -8,7 +8,7 @@ import removeIcon from '../assets/static/remove-icon.png'
 import '../assets/styles/components/CarouselItem.scss'
 
 const CarouselItem = (props) => {
-  const {id, cover, title, year, contentRating, duration } = props;
+  const {id, cover, title, year, contentRating, duration, isList } = props;
   const handleSetFavorite = () => {
     props.setFavorite({
      id, cover, title, year, contentRating, duration
@@ -25,17 +25,20 @@ const CarouselItem = (props) => {
             <img 
               className="carousel-item__details--img" 
               src={playIcon} 
-              alt="Play Icon"/> 
-            <img 
-              className="carousel-item__details--img" 
-              src={plusIcon} 
-              alt="Plus Icon"
-              onClick={handleSetFavorite}/>
-            <img 
+              alt="Play Icon"/>
+            { isList ? 
+              <img 
               className="carousel-item__details--img" 
               src={removeIcon} 
               alt="Remove Icon"
               onClick={() => handleDeleteFavorite(props.id)}/>
+            :
+              <img 
+                className="carousel-item__details--img" 
+                src={plusIcon} 
+                alt="Plus Icon"
+                onClick={handleSetFavorite}/>
+            }
           </div>
           <p className="carousel-item__details--title">{title}</p>
           <p className="carousel-item__details--subtitle">
